f99d58671819281b8244ac72792348ee89e6e893
Allen-Ronaldo-C <allenxronaldo@gmail.com>
2026-07-24 23:35:47 +0530

    Completed Cognizant Angular Exercises

--- Week - 7/Angular/student-course-portal/src/app/pages/home/home.ts
+++ Week - 7/Angular/student-course-portal/src/app/pages/home/home.ts
@@ -1,15 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { StudentService } from '../../services/student';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [FormsModule,CommonModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
-export class Home {
+export class Home implements OnInit {
 
   studentName = "Allen Ronaldo";
   college = "R.M.K College of Engineering and Technology";
@@ -21,20 +22,36 @@ export class Home {
   imageUrl = "https://angular.dev/assets/images/press-kit/angular_icon_gradient.gif";
   buttonText = "Register Course";
   isDisabled = false;
-  isPlaced = false; 
+  isPlaced = false;
   courses = [
-  "Java",
-  "Spring Boot",
-  "Angular",
-  "React",
-  "Microservices"
-];
+    "Java",
+    "Spring Boot",
+    "Angular",
+    "React",
+    "Microservices"
+  ];
+
+  today = new Date();
+
+  price = 1999;
+
+  student = {
+    name: "Allen Ronaldo",
+    department: "ECE"
+  };
+
+  serviceStudent = '';
+
+  constructor(private studentService: StudentService) {}
+
+  ngOnInit() {
+    this.serviceStudent = this.studentService.getStudentName();
+  }
 
   registerCourse() {
     alert("🎉 Course Registered Successfully!");
     this.isDisabled = true;
     this.buttonText = "Registered";
-
   }
 
 }
---
+++ Week - 7/Angular/student-course-portal/src/app/pages/reactive-form/reactive-form.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormGroup, FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-reactive-form',
+  standalone: true,
+  imports: [ReactiveFormsModule],
+  templateUrl: './reactive-form.html',
+  styleUrl: './reactive-form.css',
+})
+export class ReactiveForm {
+
+  profileForm = new FormGroup({
+    name: new FormControl(''),
+    department: new FormControl('')
+  });
+
+}
